ce879d0d992b1782bd88ebd9ac3fa469dd3e12f0
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-03-30 19:53:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  })
+);
+
 // connects our backend code with the database
 
 const dbURI =
@@ -21,7 +29,7 @@ const dbURI =
     ? `mongodb+srv://peterweglo:${process.env.DB_PASS}@cluster0.zksflg6.mongodb.net/noticeBoard?retryWrites=true&w=majority&appName=Cluster0`
     : 'mongodb://localhost:27017/noticeBoard';
 
-mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
@@ -33,24 +41,16 @@ db.on('error', (err) => console.log('Error ' + err));
 
 app.use(
   session({
-    secret: process.env.SECRET,
+    secret: 'xyz',
     store: MongoStore.create(mongoose.connection),
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV == 'production',
+      // secure: process.env.NODE_ENV == 'production',
+      secure: false,
     },
   })
 );
-if (process.env.NODE_ENV !== 'production') {
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-      methods: 'GET,POST,PUT,DELETE',
-      credentials: true,
-    })
-  );
-}
 
 // add routes
 app.use('/api', require('./routes/ads.routes'));
